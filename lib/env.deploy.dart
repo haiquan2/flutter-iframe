ef8f726644450ada9b90548fef000e5bc9df05b6
haiquanng <nguyenhaiquan.data@gmail.com>
2025-09-10 00:08:54 +0700

    fix deploy

--- lib/env.deploy.dart
+++ lib/env.deploy.dart
@@ -1,9 +1,11 @@
 class Env {
   static const baseUrl = String.fromEnvironment(
     'BASE_URL',
+    defaultValue: '',
   );
 
   static const sessionsUrl = String.fromEnvironment(
     'SESSIONS_URL',
+    defaultValue: '',
   );
 }
