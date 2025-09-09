b546a0f1f6bb8bfe1e18c61439d850cf5b0ea32c
haiquanng <nguyenhaiquan.data@gmail.com>
2025-09-10 01:06:36 +0700

    fix sth

--- lib/env.dart
+++ lib/env.dart
@@ -1,2 +1,4 @@
 const String chatKey = String.fromEnvironment('baseUrl', defaultValue: '');
 const String sessionKey = String.fromEnvironment('sessionUrl', defaultValue: '');
+const String geminiApiKey = String.fromEnvironment('GEMINI_API_KEY', defaultValue: '');
+
